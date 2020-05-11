7568572de35c6dd43cadc6cc42ccdc1dfe7e42e3
RB-Codes <rasheedbader8@gmail.com>
2020-05-11 07:31:40 +0300

    this needs some edits

---
+++ app.js
@@ -0,0 +1,11 @@
+var uname = prompt ("What is your name?");
+
+if (uname = null) {
+   alert ("Please try again");
+}
+
+var age = prompt ("How old are you") + (uname)
+
+var country = prompt ("Where are you from?")
+
+var occupation = prompt ("What do you do?")
